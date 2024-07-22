036d4a81130fe7f430ea9e92f2d02620631f9dd1
DilshodbekQudratov <qudratovdilshod9@gmail.com>
2024-07-22 21:18:21 +0500

    111

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 
 
-const USERNAME = "Dilshodbek"
+let USERNAME 
+let taunch = document.querySelector('.launch')
+document.getElementById('login').addEventListener('submit', function(event) { 
+    event.preventDefault();
+    auth()
+
+});
+async function auth() {
+    let login = document.getElementsByName("login")[0].value
+     let response = await sendRequest("user", "GET",{
+        username: login
+     })
+    if (response.error){
+        let registration =  await sendRequest("user", "POST",{
+            username: login
+        })
+        if (registration.error) {
+            alert(registration.error)
+        } else {
+            USERNAME = login
+            launch.classList.add('disabled')
+            updateUserBalance()
+            localStorage.setItem('username',USERNAME)
+        }
+    } else {
+        USERNAME = login
+        launch.classList.add('disabled')
+        updateUserBalance()
+        localStorage.setItem('username',USERNAME)
+    }
+
+}
 updateUserBalance()
+
 async function updateUserBalance()  {
     let response = await sendRequest ("user", "GET", {
         username: USERNAME
@@ -14,4 +46,31 @@ async function updateUserBalance()  {
         span.innerHTML = `[${USERNAME}, ${userBalance}]`   }
 }
 
-async function 
+async function sendRequest(url, method, data) {
+    url = `https://tg-api.tehnikum.school/tehnikum_course/minesweeper/${url}`
+    
+    if(method == "POST") {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    
+        response = await response.json()
+        return response
+    } else if(method == "GET") {
+        url = url+"?"+ new URLSearchParams(data)
+        let response = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        response = await response.json()
+        return response
+    }
+}
