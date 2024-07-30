5d4c9727df44b80d61a64397327236ee55165243
DilshodbekQudratov <qudratovdilshod9@gmail.com>
2024-07-31 01:09:40 +0300

    finish

--- script.js
+++ script.js
@@ -127,6 +127,13 @@ function activateArea() {
                 event.preventDefault()
                 setFlag()
             });
+            let row = Math.trunc(i/10)
+            let column = i - row*10
+            cell.setAttribute('data-row', row)
+            cell.setAttribute('data-column', column)
+
+            cell.addEventListener('click', makeStep)
+        
         }, i * 15)
     })
 }
@@ -201,3 +208,71 @@ function setFlag(){
         }
         console.log('stopGame')
     }
+
+   async function makeStep() {
+    let cell = event.target
+    let row = +cell.getAttribute('data-row')
+    let column = +cell.getAttribute('data-column')
+
+    console.log(row,column)
+
+    let response = await sendRequest('game_step', 'POST', {
+        game_id, row, column
+    })
+
+    if(response.error){
+        alert(response.message)
+    } else {
+        updateArea(response.table)
+        if(response.status == "Ok") {
+
+        } else if(response.status == "Failed"){
+            alert("Вы проиграли")
+            gameBtn.classList.add('disabled-button')
+            gameBtn.innerHTML = "ИГРАТЬ"
+
+            setTimeout( () => {
+                cleanArea()
+                gameBtn.classList.remove('disabled-button')
+            }, 3000)
+        } else if(response.status == "Won"){
+            alert("Вы выграли")
+            updateUserBalance()
+
+            gameBtn.classList.add('disabled-button')
+            gameBtn.innerHTML = "ИГРАТЬ"
+
+            
+            setTimeout( () => {
+                cleanArea()
+                gameBtn.classList.remove('disabled-button')
+            }, 3000)
+
+        }
+    }
+   }
+
+   function updateArea(table){
+    let cells = document.querySelectorAll(".cell")
+
+    let a = 0
+    for (let i = 0; i < table.length; i++){
+        let row = table[i]
+        for(let j = 0; j < row.length; j++) {
+            let value = row[j]
+            let cell = cells[a]
+            if(value === "BOMB") {
+                cell.classList.remove('active')
+                cell.classList.add('bomb')
+            } else if(value === ""){
+
+            } else if(value ===0) {
+                cell.classList.remove('active')
+            } else if(value > 0) {
+                cell.classList.remove('active')
+                cell.innerHTML = value
+            }
+            a++
+        }
+    }
+   }
